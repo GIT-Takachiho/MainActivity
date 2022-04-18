894189bb49ce055d4450b133560dc836dd02cae1
GIT-Takachiho <okabe@takachiho-sc.co.jp>
2022-04-18 10:32:43 +0900

    Lesson7 課題提出1 （発展課題：未対応）

--- app/src/main/java/jp/techacademy/takaomi/okabe/mainactivity/WebViewActivity.kt
+++ app/src/main/java/jp/techacademy/takaomi/okabe/mainactivity/WebViewActivity.kt
@@ -3,28 +3,15 @@ package jp.techacademy.takaomi.okabe.mainactivity
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.recyclerview.widget.RecyclerView
+
 import kotlinx.android.synthetic.main.activity_web_view.*
 import android.util.Log
-import androidx.core.text.isDigitsOnly
-import java.util.*
+
 
 class WebViewActivity: AppCompatActivity(){
 
-    // 一覧画面から登録するときのコールバック（FavoriteFragmentへ通知するメソッド)
-    var onClickAddFavorite: ((Shop) -> Unit)? = null
-    // 一覧画面から削除するときのコールバック（ApiFragmentへ通知するメソッド)
-    var onClickDeleteFavorite: ((Shop) -> Unit)? = null
-
     var id: String = ""
-    var imageUrl: String = ""
-    var address: String = ""
-    var name: String = ""
     var url: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,9 +24,6 @@ class WebViewActivity: AppCompatActivity(){
         if(shop is Shop){
             id = shop.id
 //        Log.d("test", id.toString())
-            address = shop.address
-            name = shop.name
-            imageUrl = shop.logoImage
             url = if (shop.couponUrls.sp.isNotEmpty()) shop.couponUrls.sp else shop.couponUrls.pc
         }
         webView.loadUrl(url)
